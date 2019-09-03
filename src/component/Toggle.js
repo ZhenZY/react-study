ac2e46dd5a201d53e1d452edc25f0954cb6d596a
jianglizhen <jianglizhen@ruijie.com>
2019-09-03 10:01:01 +0800

    feat: 增加事件处理 Toggle.js

---
+++ src/component/Toggle.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { directive } from '@babel/types';
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: true
+    };
+
+    // 如果没有这句话，handleClick 里如果有用 this, 会undefined
+    // js 方法如果没有用 ()，如 onClick={this.handleClick}
+    // 要绑定 this
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  deleteRow() {
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
+        <br/>
+        <button onClick={this.deleteRow.bind(this)}>
+          Delete Row
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Toggle;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Toggle from './component/Toggle';
 import * as serviceWorker from './serviceWorker';
 
 // 挂载（mount）：组件第一次被渲染到 DOM 中
 // unmount: 卸载
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Toggle />, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
